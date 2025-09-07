d0578d06351e6536ade7bde9982fe0f0fe2590e0
Hyeman Lee <hyem@Hyemanui-MacBookAir.local>
2025-09-07 23:15:14 +0900

    Deploy to GitHub Pages with green card updates

---
+++ out/_next/static/chunks/438477c2dd25d7bb.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/c958696a353c5062.js",
+  "static/chunks/3c7b63e0fc7d6e6e.js",
+  "static/chunks/turbopack-5626bfb25cd8d189.js"
+])
---
+++ out/_next/static/chunks/e0eaee800c58bc8f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/e8468b34be1adec4.js",
+  "static/chunks/3c7b63e0fc7d6e6e.js",
+  "static/chunks/turbopack-130f374eb4f580fc.js"
+])
